c48769ee7738020bf2f231085c998829b0a5ede2
Wesley254 <wesleykirui2021@gmail.com>
2023-09-18 22:54:18 +0300

    fixed some errors

--- Models/BookDto.cs
+++ Models/BookDto.cs
@@ -2,17 +2,17 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace Book_Management_System.Models
 {
     public class BookDto
     {
         public int Id { get; set; }
-        public string Title { get; set; }
-        public string Description { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
 
 
-        public List<string> Author { get; set; }
-
+        public List<string> Authors { get; set; } = new List<string>();
     }
 }
